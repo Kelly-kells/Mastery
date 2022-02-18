0ddddfe12b02fad0976575f0452933e403229a5d
unknown <kellskelechi@gmail.com>
2022-02-18 23:35:32 +0100

    click-events

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export function Home() {
-    
+
+       const[change, setChange]=useState('Shop')
+
+
+    const handleClick=()=>{
+       
+        setChange('Buy')
+
+    }
 
     return (
-        <section className='Home-container'>
-         <h1> Happy</h1>
+        <section>
+
+           <div className="Home-container">
+           <img src="/images/kicks-one.jpg" alt="" />
+
+                <button onClick={(change)}> click</button>
+
+               </div> 
+         
         </section>
     )
 }
+ 
